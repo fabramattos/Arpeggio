67887924002a9210413db69b891689c63fce7f32
Felipe Mattos <fabramattos@gmail.com>
2023-12-06 22:38:42 -0300

    refactor: refatorado controller e serviço de busca

--- src/main/kotlin/br/com/primusicos/api/controller/BuscaController.kt
+++ src/main/kotlin/br/com/primusicos/api/controller/BuscaController.kt
@@ -1,5 +1,6 @@
 package br.com.primusicos.api.controller
 
+import br.com.primusicos.api.service.BuscaService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("v1")
-class BuscaController {
+class BuscaController(val service: BuscaService) {
 
     @GetMapping("/artista")
-    fun buscaArtista(@RequestParam nome : String) : String = "requisição para API agregadora feita com sucesso para: $nome"
+    fun buscaArtista(@RequestParam nome : String) : String = service.buscaArtista(nome)
 }
