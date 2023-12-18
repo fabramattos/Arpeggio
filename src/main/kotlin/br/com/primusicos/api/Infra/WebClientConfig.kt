12bcfd08a8eb2f6183fbddb4c371963ad26762a9
Felipe Mattos <fabramattos@gmail.com>
2023-12-18 17:43:34 -0300

    refactor: substituido pelo UriComponentsBuilder para melhor legibilidade. Pequeno fix no nome tratado para busca

--- src/main/kotlin/br/com/primusicos/api/Infra/WebClientConfig.kt
+++ src/main/kotlin/br/com/primusicos/api/Infra/WebClientConfig.kt
@@ -3,12 +3,21 @@ package br.com.primusicos.api.Infra
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebClientConfig {
+class WebClientConfig: WebMvcConfigurer  {
 
     @Bean
     fun webClient(): WebClient {
         return WebClient.builder().build()
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*") // Permitir acesso de qualquer origem
+            .allowedMethods("GET") // Métodos permitidos
+            .allowedHeaders("*") // Headers permitidos
     }
 }
