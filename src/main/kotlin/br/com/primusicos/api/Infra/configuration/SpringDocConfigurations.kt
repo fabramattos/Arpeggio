9e73081bea20e60b1d142e1c0221d4f78d291347
Felipe Mattos <fabramattos@gmail.com>
2024-01-17 23:05:03 -0300

    fix: tentando resolver CORS no servidor

--- src/main/kotlin/br/com/primusicos/api/Infra/configuration/SpringDocConfigurations.kt
+++ src/main/kotlin/br/com/primusicos/api/Infra/configuration/SpringDocConfigurations.kt
@@ -5,6 +5,10 @@ import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
 
 @Configuration
 class SpringDocConfigurations {
@@ -25,5 +29,21 @@ class SpringDocConfigurations {
 //                url = "urlFrontEnd.com.br"
             }
         })
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+
+        source.registerCorsConfiguration("/doc/swagger-ui", config)
+        return CorsFilter(source)
+    }
+
+
 
 }
