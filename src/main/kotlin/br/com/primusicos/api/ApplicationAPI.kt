494ea28260f21a2b191746993ae49a80ff44b11c
Felipe Mattos <fabramattos@gmail.com>
2024-01-26 15:51:51 -0300

    fix:  - inserida anotação para permitir agendamento de métdos

--- src/main/kotlin/br/com/primusicos/api/ApplicationAPI.kt
+++ src/main/kotlin/br/com/primusicos/api/ApplicationAPI.kt
@@ -2,8 +2,10 @@ package br.com.primusicos.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class ApplicationAPI
 
 fun main(args: Array<String>) {
